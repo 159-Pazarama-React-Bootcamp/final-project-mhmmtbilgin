f3a2bcde571e72b7bfedd03ccdebaa18307556c8
Muhammet BİLGİN <muhammet.bilgin@mobven.com>
2022-01-28 01:19:53 +0300

    Admin Login Added
    Admin login formu oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import FormPage from "./views/Form/FormPage/FormPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormSubmitPage from "./views/Form/FormSubmitPage/FormSubmitPage";
+import Login from "./views/Admin/Login/Login";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/basvuru-olustur" element={<FormPage />} />
             <Route path="/basvuru-basarili" element={<FormSubmitPage />} />
+            <Route path="/admin" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </header>
