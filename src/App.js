d014c4113d2f050bf0628cf1198ffa62ae5b5daf
Muhammet BİLGİN <muhammet.bilgin@mobven.com>
2022-01-27 23:22:34 +0300

    FormSubmitPage Added
    Form doldurulup gönderildiğinde yönlendirilecek sayfa eklendi.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import FormPage from "./views/Form/FormPage/FormPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import FormSubmitPage from "./views/Form/FormSubmitPage/FormSubmitPage";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+     
         <BrowserRouter>
           <Routes>
             <Route path="/basvuru-olustur" element={<FormPage />} />
+            <Route path="/basvuru-basarili" element={<FormSubmitPage />} />
           </Routes>
-          
         </BrowserRouter>
       </header>
     </div>
---
+++ src/components/ThanksPage/Thanks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.css"
+
+export default function Thanks(props) {
+    const { label, imgURL } = props;
+    return <div className='thanks'>
+        <img className='thanks__image' src={imgURL} />
+        <span className='thanks__label'>{label}</span>
+    </div>;
+}
